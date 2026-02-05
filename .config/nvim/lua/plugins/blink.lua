273578b77602d36766547b23d9ffaf0ff96f2bdd
Haaris Rahman <122410226+haarisr@users.noreply.github.com>
2026-02-04 18:04:06 -0800

    Updates (#5)

--- .config/nvim/lua/plugins/blink.lua
+++ .config/nvim/lua/plugins/blink.lua
@@ -1,6 +1,7 @@
 return {
     "saghen/blink.cmp",
-    version = "v0.*",
+    event= 'VimEnter',
+    version = "v1.*",
     enabled = true,
     dependencies = { "L3MON4D3/LuaSnip", version = "v2.*" },
     opts = {
